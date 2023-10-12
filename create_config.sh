56ce18e3e99269f8cdb724ff399583dfdb481355
MokraneDevOps <147703856+MokraneDevOps@users.noreply.github.com>
2023-10-12 16:52:21 +0200

    Update create_config.sh
    ajouter html sur le document Root

--- create_config.sh
+++ create_config.sh
@@ -4,7 +4,7 @@ cat <<EOL > /etc/apache2/sites-available/nexcloud.conf
 <VirtualHost *:80>
     ServerAdmin mamar.mmoumouni@gmail.com
     ServerName next.me
-    DocumentRoot /var/www/nextcloud
+    DocumentRoot /var/www/html/nextcloud
 
     ErrorLog /var/www/nextcloud/error.log
     CustomLog /var/www/nextcloud/access.log combined
@@ -23,7 +23,7 @@ cat <<EOL > /etc/apache2/sites-available/wordpress.conf
 <VirtualHost *:80>
     ServerAdmin mamar.mmoumouni@gmail.com
     ServerName press.me
-    DocumentRoot /var/www/wordpress
+    DocumentRoot /var/www/html/wordpress
 
     ErrorLog /var/www/wordpress/error.log
     CustomLog /var/www/wordpress/access.log combined
@@ -42,7 +42,7 @@ cat <<EOL > /etc/apache2/sites-available/prestashop.conf
 <VirtualHost *:80>
     ServerAdmin mamar.mmoumouni@gmail.com
     ServerName shop.me
-    DocumentRoot /var/www/prestashop/
+    DocumentRoot /var/www/html/prestashop/
 
     ErrorLog /var/www/prestashop/error.log
     CustomLog /var/www/prestashop/access.log combined
